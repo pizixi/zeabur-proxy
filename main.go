59e0bcc876ba573300a0383e8bf18e2ce71bdea1
yann <pizixi125@gmail.com>
2023-08-20 10:05:53 +0800

    2

--- main.go
+++ main.go
@@ -15,24 +15,8 @@ func main() {
 
 	r := gin.Default()
 	r.Any("/*any", func(c *gin.Context) {
-		SetCORS(c)
 		proxy.ServeHTTP(c.Writer, c.Request)
 	})
 
 	r.Run() // listen and serve on 0.0.0.0:8080
-}
-
-// SetCORS SetCORS
-func SetCORS(c *gin.Context) {
-	c.Header("Access-Control-Allow-Origin", "*")
-	c.Writer.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type, x-requested-with")
-	c.Header("Content-Type", "text/event-stream")
-	c.Header("Cache-Control", "no-cache")
-	c.Header("Connection", "keep-alive")
-
-	// c.Writer.Header().Set("Access-Control-Allow-Origin", "*")
-	// c.Writer.Header().Set("Access-Control-Allow-Headers", "Authorization, Content-Type, x-requested-with")
-	// c.Writer.Header().Set("Access-Control-Allow-Methods", "POST, OPTIONS")
-	// c.Writer.Header().Set("Access-Control-Max-Age", "86400")
-	// c.Writer.Header().Set("Content-Type", "application/json")
 }
