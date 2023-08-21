fbc80903c8265dba72fb8ba2ed0e833d47d0df44
yann <pizixi125@gmail.com>
2023-08-21 15:01:09 +0800

    回到起点

--- main.go
+++ main.go
@@ -4,7 +4,6 @@ import (
 	"net/http/httputil"
 	"net/url"
 	"os"
-	"strings"
 
 	"github.com/gin-gonic/gin"
 )
@@ -16,11 +15,6 @@ func main() {
 
 	r := gin.Default()
 	r.Any("/*any", func(c *gin.Context) {
-		// 检查请求头中的"Accept"字段
-		acceptHeader := c.GetHeader("Accept")
-		if strings.Contains(acceptHeader, "application/json") {
-			c.Header("Content-Type", "application/json")
-		}
 		proxy.ServeHTTP(c.Writer, c.Request)
 	})
 
