52e95d272bc5d905bba68fe9b43535cd4ab8f80f
pizixi <56673215+pizixi@users.noreply.github.com>
2023-09-11 16:44:52 +0800

    Update main.go

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"net/http"
 	"net/http/httputil"
 	"net/url"
 	"os"
@@ -9,14 +10,48 @@ import (
 )
 
 func main() {
-	proxyURL := os.Getenv("Proxy_URL")
-	target, _ := url.Parse(proxyURL)
-	proxy := httputil.NewSingleHostReverseProxy(target)
-
 	r := gin.Default()
-	r.Any("/*any", func(c *gin.Context) {
+
+	proxySites := map[string]string{
+		"/reproxy-workergpt/*any": "https://workergpt.cn",
+		"/reproxy-eqing/*any":     "https://next.eqing.tech",
+		"/reproxy-miyadns/*any":   "https://www.miyadns.com",
+		"/reproxy-lbbai/*any":     "https://postapi.lbbai.cc",
+		"/reproxy-mixerbox/*any":  "https://chatai.mixerbox.com",
+	}
+
+	for routePath, targetURL := range proxySites {
+		target, _ := url.Parse(targetURL)
+		proxy := httputil.NewSingleHostReverseProxy(target)
+
+		// 修改代理响应
+		proxy.ModifyResponse = func(res *http.Response) error {
+			// 添加CORS响应头
+			res.Header.Set("Access-Control-Allow-Origin", "*")
+			res.Header.Set("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS")
+			res.Header.Set("Access-Control-Allow-Headers", "Origin, Content-Type, Accept, Authorization, X-Requested-With")
+			res.Header.Set("Content-Type", "text/event-stream")
+			res.Header.Set("Cache-Control", "no-cache")
+			res.Header.Set("Connection", "keep-alive")
+			return nil
+		}
+
+		r.Any(routePath, func(c *gin.Context) {
+			// 附加原始请求路径到目标 URL
+			c.Request.URL.Path = target.Path + c.Param("any")
+			// 设置请求的Host头
+			c.Request.Host = target.Host
+
+			proxy.ServeHTTP(c.Writer, c.Request)
+		})
+	}
+
+	// 处理未匹配到的路径
+	r.NoRoute(func(c *gin.Context) {
+		target, _ := url.Parse(os.Getenv("Proxy_URL"))
+		proxy := httputil.NewSingleHostReverseProxy(target)
 		proxy.ServeHTTP(c.Writer, c.Request)
 	})
 
-	r.Run() // listen and serve on 0.0.0.0:8080
+	r.Run(":" + os.Getenv("PORT")) // 在0.0.0.0:8899 上监听并在此处服务
 }
